727efe602c26d902ea6aeb35d8d7640de7cf0f5a
kadir erdem <kadirerdem58@hotmail.com>
2022-09-27 14:37:25 +0300

    hafta2 eklendi

---
+++ src/hafta2/denemegit.java
@@ -0,0 +1,10 @@
+package hafta2;
+
+public class denemegit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdsadsa git denemesi xd");
+	}
+
+}
